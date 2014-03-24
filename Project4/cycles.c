855370339491f203a8d88ced6f9a8f4bdf1ab54f
Stuart Olsen <so1132@txstate.edu>
2014-03-23 20:54:06 -0500

    Cleaned up signedness, etc.

--- Project4/cycles.c
+++ Project4/cycles.c
@@ -8,28 +8,48 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
-#include <assert.h>
+#include <limits.h>
 
-#define MEMSIZE 1048576
-#define ARRAYLEN(NAME) (sizeof (NAME) / sizeof (*NAME))
+typedef intmax_t integer;
+#define PR_INTEGER PRIiMAX
 
-typedef uint32_t word;
-typedef int32_t sword;
-typedef uint64_t dword;
-typedef int_least64_t integer;
-#define PR_INTEGER PRIdLEAST64
+// Extracts an the unsigned value on [low, high)
+static inline
+uint32_t BITRANGE (uint32_t value,
+                   uint_fast8_t low,
+                   uint_fast8_t high)
+{
+  return (value >> low) & ((1 << (high - low)) - 1);
+}
 
-static word icount, *instruction;
-static word mem [MEMSIZE / 4];
+// Extends the highest bit on [0, bits)
+static inline
+uint32_t SIGN_EXTEND (uint32_t value,
+                      uint_fast8_t bits)
+{
+  if (value & (1 << (bits - 1))) // Highest bit 1
+    return value | (-1 << bits);
+  else
+    return value;
+}
 
 
 
-static word Convert(word x)
+enum {
+  MEMSIZE = 1048576
+};
+
+static uint32_t icount, *instruction;
+static uint32_t mem [MEMSIZE / 4];
+
+
+
+static uint32_t Convert(uint32_t x)
 {
   return (x >> 24) | ((x >> 8) & 0xff00) | ((x << 8) & 0xff0000) | (x << 24);
 }
 
-static word Fetch(word pc)
+static uint32_t Fetch(uint32_t pc)
 {
   pc = (pc - 0x00400000) >> 2;
   if (pc >= icount) {
@@ -39,7 +59,7 @@ static word Fetch(word pc)
   return instruction[pc];
 }
 
-static word LoadWord(word addr)
+static uint32_t LoadWord(uint32_t addr)
 {
   if ((addr & 3) != 0) {
     fprintf(stderr, "unaligned data access\n");
@@ -53,7 +73,7 @@ static word LoadWord(word addr)
   return mem[addr / 4];
 }
 
-static void StoreWord(word data, word addr)
+static void StoreWord(uint32_t data, uint32_t addr)
 {
   if ((addr & 3) != 0) {
     fprintf(stderr, "unaligned data access\n");
@@ -69,7 +89,7 @@ static void StoreWord(word data, word addr)
 
 
 
-enum {
+enum opcode {
   FUNCTION = 0x00,
   J        = 0x02,
   JAL      = 0x03,
@@ -83,7 +103,7 @@ enum {
   SW       = 0x2B
 };
 
-enum {
+enum funcode {
   SLL  = 0x00,
   SRA  = 0x03,
   JR   = 0x08,
@@ -96,120 +116,97 @@ enum {
   SLT  = 0x2a
 };
 
-enum {
+enum trapcode {
   NEWLINE = 0x00,
   PRINT   = 0x01,
   PROMPT  = 0x05,
   STOP    = 0x0a
 };
 
-const char regname[32][6] = {"$zero","$at","$v0","$v1","$a0","$a1","$a2","$a3",
-                             "$t0","$t1","$t2","$t3","$t4","$t5","$t6","$t7","$s0","$s1","$s2","$s3",
-                             "$s4","$s5","$s6","$s7","$t8","$t9","$k0","$k1","$gp","$sp","$fp","$ra"};
+enum regid {
+  ZERO = 0,
+  AT   = 1,
+  V0   = 2,
+  V1   = 3,
+  A0   = 4,
+  A1   = 5,
+  A2   = 6,
+  A3   = 7,
+  T0   = 8,
+  T1   = 9,
+  T2   = 10,
+  T3   = 11,
+  T4   = 12,
+  T5   = 13,
+  T6   = 14,
+  T7   = 15,
+  S0   = 16,
+  S1   = 17,
+  S2   = 18,
+  S3   = 19,
+  S4   = 20,
+  S5   = 21,
+  S6   = 22,
+  S7   = 23,
+  T8   = 24,
+  T9   = 25,
+  K0   = 26,
+  K1   = 27,
+  GP   = 28,
+  SP   = 29,
+  FP   = 30,
+  RA   = 31,
+  LO   = 32,
+  HI   = 33,
+  REGS = 1 + HI - ZERO
+};
 
-static void Decode(int pc, int instr)  // do not make any changes outside of this function
-{
-  #ifndef CHAR_BIT
-  # define CHAR_BIT 8
-  #endif
+enum pipestage {
+  IF1    = 0,
+  IF2    = 1,
+  ID     = 2,
+  EXE1   = 3,
+  EXE2   = 4,
+  MEM1   = 5,
+  MEM2   = 6,
+  MEM3   = 7,
+  WB     = 8,
+  STAGES = 1 + WB - IF1
+};
 
-  // Reinterpret the bit sequence defined by [low, high) on `instr'
-  #define BITRANGE(value, low, high) (((value) >> low) & ((1 << (high - low)) - 1))
-  #define SIGN_EXTEND(value, bits) (((signed int) (value) << (sizeof (int) * CHAR_BIT - bits)) >> (sizeof (int) * CHAR_BIT - bits))
-
-  unsigned int opcode = BITRANGE (instr, 26, 32);
-  unsigned int rs     = BITRANGE (instr, 21, 26);
-  unsigned int rt     = BITRANGE (instr, 16, 21);
-  unsigned int rd     = BITRANGE (instr, 11, 16);
-  unsigned int shamt  = BITRANGE (instr, 6, 11);
-  unsigned int funct  = BITRANGE (instr, 0, 6);
-  unsigned int imm    = BITRANGE (instr, 0, 16);
-  signed   int simm   = SIGN_EXTEND (imm, 16);
-  unsigned int addr   = BITRANGE (instr, 0, 26);
-  unsigned int jaddr  = ((pc + 4) & 0xf0000000) + (addr << 2); // Jump address, adjusted for printing
-  unsigned int baddr  = pc + (simm << 2) + 4; // Branch address, adjusted for printing
-
-  #undef SIGN_EXTEND
-  #undef BITRANGE
-
-  const char* RD = regname [rd];
-  const char* RS = regname [rs];
-  const char* RT = regname [rt];
-
-  #define PRINT(format, ...) (fprintf (stderr, "%8x: "format"\n", pc, ##__VA_ARGS__))
-
-  switch (opcode) {
-    case 0x00:
-      switch (funct) {
-        //   FUNCTION     FORMAT           NAME    ARGUMENTS
-        case 0x00: PRINT ("%s %s, %s, %u", "sll",  RD, RS, shamt); break;
-        case 0x03: PRINT ("%s %s, %s, %u", "sra",  RD, RS, shamt); break;
-        case 0x08: PRINT ("%s %s",         "jr",   RS);            break;
-        case 0x10: PRINT ("%s %s",         "mfhi", RD);            break;
-        case 0x12: PRINT ("%s %s",         "mflo", RD);            break;
-        case 0x18: PRINT ("%s %s, %s",     "mult", RS, RT);        break;
-        case 0x1a: PRINT ("%s %s, %s",     "div",  RS, RT);        break;
-        case 0x21: PRINT ("%s %s, %s, %s", "addu", RD, RS, RT);    break;
-        case 0x23: PRINT ("%s %s, %s, %s", "subu", RD, RS, RT);    break;
-        case 0x2a: PRINT ("%s %s, %s, %s", "slt",  RD, RS, RT);    break;
-        default:   PRINT ("unimplemented");
-      }
-      break;
-    //   OPCODE       FORMAT           NAME     ARGUMENTS
-    case 0x02: PRINT ("%s %x",         "j",     jaddr);         break;
-    case 0x03: PRINT ("%s %x",         "jal",   jaddr);         break;
-    case 0x04: PRINT ("%s %s, %s, %x", "beq",   RS, RT, baddr); break;
-    case 0x05: PRINT ("%s %s, %s, %x", "bne",   RS, RT, baddr); break;
-    case 0x09: PRINT ("%s %s, %s, %d", "addiu", RT, RS, simm);  break;
-    case 0x0c: PRINT ("%s %s, %s, %u", "andi",  RT, RS, imm);   break;
-    // We should be using unsigned here per the MIPS spec, but we need signed to pass the testcases.
-    case 0x0f: PRINT ("%s %s, %d",     "lui",   RT, simm);      break;
-    case 0x1a: PRINT ("%s %x",         "trap",  addr);          break;
-    case 0x23: PRINT ("%s %s, %d(%s)", "lw",    RT, simm, RS);  break;
-    case 0x2b: PRINT ("%s %s, %d(%s)", "sw",    RT, simm, RS);  break;
-    default:   PRINT ("unimplemented");
-  }
-
-  #undef PRINT
-}
-
-static void Interpret(int start)
+static void Interpret (uint32_t start)
 {
   // Registers
-  word pc = start;
-  word reg [32] = {
-    [28] = 0x10008000, // $gp
-    [29] = 0x10000000 + MEMSIZE // $sp
+  uint32_t pc = start;
+  uint32_t reg [REGS] = {
+    [GP] = 0x10008000,
+    [SP] = 0x10000000 + MEMSIZE
   };
-  word lo, hi;
-  dword wide;
 
   // Control data
   integer count = 0;
 
-  #define REG_HILO 33
-  #define RREAD(REG) fprintf (stderr, "%s => 0x%x\n", REG == REG_HILO ? "HILO" : regname [REG], REG == REG_HILO ? lo : reg [REG])
-  #define RWRITE(REG) fprintf (stderr, "%s <= 0x%x\n", REG == REG_HILO ? "HILO" : regname [REG], REG == REG_HILO ? lo : reg [REG])
+  #define RREAD(REG)
+  #define RWRITE(REG)
 
   // Main loop
   while (1) {
-    word instr  = Fetch(pc);
-    Decode (pc, instr);
-    reg [0] = 0; // $zero
+    uint32_t instr  = Fetch(pc);
+    reg [ZERO] = 0; // $zero
     pc += 4;
     ++count;
 
-    uint8_t  opcode = instr >> 26;
-    uint8_t  rs     = (instr >> 21) & 0x1f;
-    uint8_t  rt     = (instr >> 16) & 0x1f;
-    uint8_t  rd     = (instr >> 11) & 0x1f;
-    uint8_t  shamt  = (instr >> 6) & 0x1f;
-    uint8_t  funct  = instr & 0x3f;
-    uint16_t uimm   = instr & 0xffff;
-    int16_t  simm   = uimm;
-    word     addr   = instr & 0x3ffffff;
-    word     jaddr  = (pc & 0xf0000000) + addr * 4;
-    word     baddr  = pc + simm * 4;
+    uint8_t  opcode = BITRANGE (instr, 26, 32);
+    uint8_t  rs     = BITRANGE (instr, 21, 26);
+    uint8_t  rt     = BITRANGE (instr, 16, 21);
+    uint8_t  rd     = BITRANGE (instr, 11, 16);
+    uint8_t  shamt  = BITRANGE (instr, 6, 11);
+    uint8_t  funct  = BITRANGE (instr, 0, 6);
+    uint16_t uimm   = BITRANGE (instr, 0, 16);
+    int16_t  simm   = SIGN_EXTEND (uimm, 16);
+    uint32_t addr   = BITRANGE (instr, 0, 26);
+    uint32_t jaddr  = (pc & 0xf0000000) + addr * 4;
+    uint32_t baddr  = pc + simm * 4;
 
     switch (opcode)
     {
@@ -224,7 +221,7 @@ static void Interpret(int start)
 
           case SRA:
             RREAD (rs);
-            reg [rd] = reg [rs] >> shamt;
+            reg [rd] = SIGN_EXTEND (reg [rs] >> shamt, 32 - shamt);
             RWRITE (rd);
             break;
 
@@ -234,40 +231,40 @@ static void Interpret(int start)
             break;
 
           case MFHI:
-            RREAD (REG_HILO);
-            reg [rd] = hi;
+            RREAD (HI);
+            reg [rd] = reg [HI];
             RWRITE (rd);
             break;
 
           case MFLO:
-            RREAD (REG_HILO);
-            reg [rd] = lo;
+            RREAD (LO);
+            reg [rd] = reg [LO];
             RWRITE (rd);
             break;
 
           case MULT:
             RREAD (rs);
             RREAD (rt);
-            wide = reg [rs] * reg [rt];
-            lo = wide & 0xffffffff;
-            hi = wide >> 32;
-            RWRITE (REG_HILO);
+            uint64_t wide = reg [rs] * reg [rt];
+            reg [LO] = wide & 0xffffffff;
+            reg [HI] = wide >> 32;
+            RWRITE (LO);
+            RWRITE (HI);
             break;
 
           case DIV:
             RREAD (rs);
             RREAD (rt);
-            if (reg [rt] == 0)
-            {
-              fprintf (stderr, "division by zero: pc = 0x%x\n", pc-4);
+            if (reg [rt] == 0) {
+              fprintf (stderr, "division by zero: pc = 0x%"PRIx32"\n", pc - 4);
               goto halt;
             }
-            else
-            {
-              lo = reg [rs] / reg [rt];
-              hi = reg [rs] % reg [rt];
+            else {
+              reg [LO] = reg [rs] / reg [rt];
+              reg [HI] = reg [rs] % reg [rt];
             }
-            RWRITE (REG_HILO);
+            RWRITE (LO);
+            RWRITE (HI);
             break;
 
           case ADDU:
@@ -287,12 +284,12 @@ static void Interpret(int start)
           case SLT:
             RREAD (rs);
             RREAD (rt);
-            reg [rd] = ((sword) reg [rs] < (sword) reg [rt] ? 1 : 0);
+            reg [rd] = ((int32_t) reg [rs] < (int32_t) reg [rt] ? 1 : 0);
             RWRITE (rd);
             break;
 
           default:
-            fprintf (stderr, "unimplemented instruction: pc = 0x%x\n", pc-4);
+            fprintf (stderr, "unimplemented instruction: pc = 0x%"PRIx32"\n", pc - 4);
             goto halt;
         }
         break;
@@ -334,7 +331,7 @@ static void Interpret(int start)
         break;
 
       case LUI:
-        reg [rt] = simm << 16;
+        reg [rt] = uimm << 16;
         RWRITE (rt);
         break;
 
@@ -353,37 +350,35 @@ static void Interpret(int start)
           case PROMPT:
             printf ("\n? ");
             fflush (stdout);
-            scanf ("%"PRIu32, &reg [rt]);
+            int32_t input;
+            scanf ("%"PRIi32, &input);
+            reg [rt] = input;
             RWRITE (rt);
             break;
 
           case STOP:
             goto halt;
-            break;
 
           default:
-            fprintf (stderr, "unimplemented trap: pc = 0x%x\n", pc-4);
+            fprintf (stderr, "unimplemented trap: pc = 0x%"PRIx32"\n", pc - 4);
             goto halt;
         }
         break;
 
       case LW:
         RREAD (rs);
-        fprintf (stderr, "0x%"PRIx32": Load %"PRIi16"(0x%"PRIx32") => ", pc-4, simm, reg [rs]);
         reg [rt] = LoadWord (reg [rs] + simm);
-        fprintf (stderr, "0x%"PRIx32"\n", reg [rt]);
         RWRITE (rt);
         break;
 
       case SW:
         RREAD (rs);
         RREAD (rt);
-        fprintf (stderr, "0x%"PRIx32": Store %"PRIi16"(0x%"PRIx32") <= 0x%"PRIx32"\n", pc-4, simm, reg [rs], reg [rt]);
         StoreWord (reg [rt], reg [rs] + simm);
         break;
 
       default:
-        fprintf (stderr, "unimplemented instruction: pc = 0x%x\n", pc-4);
+        fprintf (stderr, "unimplemented instruction: pc = 0x%"PRIx32"\n", pc - 4);
         goto halt;
     }
   }
@@ -392,9 +387,11 @@ halt:
   ;
 }
 
+
+
 int main(int argc, char *argv[])
 {
-  word c, start;
+  uint32_t c, start;
   int little_endian;
   FILE *f;
 
@@ -418,7 +415,7 @@ int main(int argc, char *argv[])
     start = Convert(start);
   }
 
-  instruction = (word *)(malloc(icount * 4));
+  instruction = (uint32_t *)(malloc(icount * 4));
   if (instruction == NULL) {fprintf(stderr, "error: out of memory\n"); exit(-1);}
   c = fread(instruction, 4, icount, f);
   if (c != icount) {fprintf(stderr, "error: could not read (all) instructions from file %s\n", argv[1]); exit(-1);}
